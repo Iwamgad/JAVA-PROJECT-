76da48038fed192a058e00e9b91b914553ab8247
Iwamgad <dagmawiharry2@gmail.com>
2023-06-29 14:34:01 +0300

    I added a controller package

---
+++ Spring-Boot-Project/src/main/java/com/portfolio/SpringBoot/Project/controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.portfolio.SpringBoot.Project.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+}
