021b829ed3579c99da7d0c7e53790228ac5aec83
Iwamgad <dagmawiharry2@gmail.com>
2023-06-29 15:32:53 +0300

    I edited some of the dependencies

--- Spring-Boot-Project/src/main/java/com/portfolio/SpringBoot/Project/controller/HelloController.java
+++ Spring-Boot-Project/src/main/java/com/portfolio/SpringBoot/Project/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.portfolio.SpringBoot.Project.controller;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Welcome to Dagis Code !! aaahhh";
+    }
 }
