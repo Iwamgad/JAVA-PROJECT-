a0b1a8ce89361aa8352f3870ff13b345a3c3cc3c
Iwamgad <dagmawiharry2@gmail.com>
2023-07-04 22:01:51 +0300

    I edited some files

--- Spring-Boot-Project/src/main/java/com/portfolio/SpringBoot/Project/service/DepartmentService.java
+++ Spring-Boot-Project/src/main/java/com/portfolio/SpringBoot/Project/service/DepartmentService.java
@@ -1,5 +1,22 @@
 package com.portfolio.SpringBoot.Project.service;
 
+import com.portfolio.SpringBoot.Project.entity.Department;
+import com.portfolio.SpringBoot.Project.error.DepartmentNotFoundException;
+
+import java.util.List;
+
 public interface DepartmentService {
 
+    public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
 }
